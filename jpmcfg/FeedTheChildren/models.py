a07551b4f92a86f557519cad58510fdd5daf0ee2
bing <bing4130@gmail.com>
2015-10-17 00:22:48 -0400

    Added models

--- jpmcfg/FeedTheChildren/models.py
+++ jpmcfg/FeedTheChildren/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Person(models.Model):
+  username = models.ForeignKey(User)
+  p_type = models.CharField(max_length=30)
+  
+class Customers(models.Model):
+  snap = models.CharField(max_length=30)
+  home_loc = models.CharField(max_length=30)
+  distance_rad = models.IntegerField()
+  
+class Vendor(models.Model):
+  store_name = models.CharField(max_length=30)
+  location = models.CharField(max_length=30)
+  hours = models.CharField(max_length=30)
