5ea51ef82ef8291675c7fab30b3ae42e0bfeed83
bing <bing4130@gmail.com>
2015-10-17 01:19:02 -0400

    deleted Person class and combined with customers and vendor

--- jpmcfg/FeedTheChildren/models.py
+++ jpmcfg/FeedTheChildren/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Person(models.Model):
-  username = models.ForeignKey(User)
-  p_type = models.CharField(max_length=30)
   
 class Customers(models.Model):
   snap = models.CharField(max_length=30)
   home_loc = models.CharField(max_length=30)
   distance_rad = models.IntegerField()
+  username = models.ForeignKey(User)
   
 class Vendor(models.Model):
   store_name = models.CharField(max_length=30)
   location = models.CharField(max_length=30)
   hours = models.CharField(max_length=30)
+  username = models.ForeignKey(User)
