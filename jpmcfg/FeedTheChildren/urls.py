7d925bd98c4fb313e14ba901692725e13c034f35
Derek <ddy214@nyu.edu>
2015-10-17 11:29:19 -0400

    url, template, and view changes

--- jpmcfg/FeedTheChildren/urls.py
+++ jpmcfg/FeedTheChildren/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^dddddd$', views.createUser, name='Registration'),
+  url(r'^createUser$', views.createUser, name='Registration'),
   url(r'^$', views.home, name='Home'),
-
+  url(r'^login$', views.loginUser, name="Login"),
 ]
